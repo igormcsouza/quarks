f5b86838243febf5f9ddd58a38ec4cb8e226549c
Igor Souza <igormcsouza@gmail.com>
2021-03-14 17:55:26 -0300

    Add 400 error handling test

--- backend/tests/test_blueprints/test_classifier.py
+++ backend/tests/test_blueprints/test_classifier.py
@@ -7,3 +7,12 @@ def test_classifier_predict(client):
 
     assert r.status_code == 200
     assert r.json.get('result') in ['pos', 'neg']
+
+def test_classifier_predict_400(client):
+    r = client.post(
+        '/classifier/predict',
+        json={},
+        headers={ "Content-Type": "application/json" }
+    )
+
+    assert r.status_code == 400
